7ffd3ddaf41144771f7ce0320d5b87bc488535af
Simon Stier <52674635+simontaurus@users.noreply.github.com>
2023-05-12 03:44:10 +0200

    replace datetime with timestamp

--- PageExchange.sql
+++ PageExchange.sql
@@ -2,7 +2,7 @@
 -- If a package is uninstalled, its entry gets deleted.
 CREATE TABLE /*_*/px_packages (
 	pxp_id int PRIMARY KEY,
-	pxp_date_installed datetime NOT NULL,
+	pxp_date_installed timestamp NOT NULL,
 	pxp_installing_user int NOT NULL,
 	pxp_name varchar(255) NOT NULL,
 	pxp_global_id varchar(255) NOT NULL UNIQUE,
